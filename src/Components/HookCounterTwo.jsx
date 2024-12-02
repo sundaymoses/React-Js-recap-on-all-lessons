fd7abfb40f7df83e78ce07d70273125e73d51676
Sunday Moses <sundaym@syncfusion.com>
2024-12-02 17:10:24 +0300

    updated states

--- src/Components/HookCounterTwo.jsx
+++ src/Components/HookCounterTwo.jsx
@@ -3,12 +3,17 @@ import React, { useState } from "react";
 function HookCounterTwo() {
   const initialCount = 0;
   const [count, setCount] = useState(initialCount);
+  const IncreaseByFive =() =>{
+    for(i=1)
+    setCount(prevCount => prevCount+1);
+  }
   return (
     <div>
       {count}
       <button onClick={() => setCount(count + 1)}>Increement</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
       <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={IncreaseByFive}>Increse by 5</button>
     </div>
   );
 }
